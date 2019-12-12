67f42228f08ffef47b66a265481813ade490c919
Axel Viala <axel.viala@darnuria.eu>
2019-12-12 14:01:33 +0100

    Some changes

--- ex01.c
+++ ex01.c
@@ -1,6 +1,7 @@
 #include <pthread.h>
 #include <stdio.h>   // printf etc.
 #include <stdlib.h>
+#include <stdint.h>
 
 ///////////////////////////////////////////////////////////////////////////////
 // Objectif du TP - découvrir les bases de la programmation concurrente et
@@ -15,21 +16,21 @@
 // - Corriger les erreurs de compilation (pourquoi? Pour vous faire lire le fichier)
 // - Lancer plusieurs fois ce code observer les différences entre 8 lancements
 // - Commenter les fonctions dans le style des docstrings (resemble bcp au jsdoc).
-// - 
+// -
 ///////////////////////////////////////////////////////////////////////////////
 
 // Shared_counter functions and type definitions.
 typedef struct _shared_counter_t {
-    int counter;
+    int32_t counter;
 } shared_counter_t;
 
 shared_counter_t * shared_counter_new(void);
 void shared_counter_drop(shared_counter_t sc);
-shared_counter_increment(shared_counter_t * shared_counter);
-int shared_counter_value(const shared_counter_t * shared_counter);
+shared_counter_increment(shared_counter_t * sc);
+int32_t shared_counter_value(const shared_counter_t * sc);
 
 typedef struct _thread_data_t {
-  int thread_id;
+  int32_t thread_id;
   shared_counter_t * shared_counter;
 } thread_data_t;
 
@@ -54,25 +55,26 @@ void shared_counter_drop(shared_counter_t * sc) {
     free(sc);
 }
 
-void shared_counter_increment(shared_counter_t * shared_counter) {
+void shared_counter_increment(shared_counter_t * sc) {
     // TODO: Might need something like a ???.
-    shared_counter->counter += 1;
+    sc->counter += 1;
 }
 
-int shared_counter_value(const shared_counter_t * shared_counter) {
+int shared_counter_value(const shared_counter_t * sc) {
     // TODO: Read need something or not?
-    return shared_counter->counter;
+    return sc->counter;
 }
 ///////////////////////////////////////////////////////////////////////////////
 
 // Main program.
 
 #define MAX_THREADS 3
+
 void * run(void * args) {
     thread_data_t * data = (thread_data_t *) args;
     printf("Bonjour depuis le thread: %i.\n", data->thread_id);
     // Let's work thread! Increment counter 10000 times.
-    for (int i = 0; i < 10000; i++) {
+    for (int32_t i = 0; i < 10000; i++) {
         shared_counter_increment(data->shared_counter);
     }
     // End our thread it worked well, can gain some rest.
@@ -82,34 +84,32 @@ void * run(void * args) {
 int main(void) {
     printf("The main thread is started\n");
 
-    // On crée un tableau pour les structures pthreads.
+    // Create an array for thread struct
     pthread_t threads[MAX_THREADS];
-    // on crée un tableau pour les data passées au thread.
+    // Array for data passed to threads
     thread_data_t thr_data[MAX_THREADS];
 
-    // On crée une ressource partagée ici un compteur partagé
-    // Et on va découvrir les problèmes de partager sans politique de partage ;)
+    // Create shared ressource: a shared counter
+    // Let's discovers joy and pitfalls of concurrent programming ;)
     shared_counter_t * shared_counter;
     shared_counter_new();
 
-    // On va créer `MAX_THREADS` threads
-    for (int i = 0; i < MAX_THREADS; i += 1) {
+    // Create `MAX_THREADS` threads
+    for (int32_t i = 0; i < MAX_THREADS; i += 1) {
         thr_data[i].thread_id = i;
-        // On passe un pointeur sur shared_counter
+        // Take a pointer to shared counter
         thr_data[i].shared_counter = &shared_counter;
-        // Petit bug à la ligne suivante checkez vos erreurs de compilation
-        int failure_reason = pthread_create(&threads[i], NULL, run, &thr_data[i]);
+
+        printf("Création du thread %i\n", i);
+        int32_t failure_reason = pthread_create(&threads[i], NULL, run, &thr_data[i]);
         if (failure_reason) {
             fprintf(stderr, "Error: pthread_create, rc: %d\n", failure_reason);
             return EXIT_FAILURE;
         }
-        printf("Création du thread %i\n", i);
     }
 
     // Joins threads, block until they finished their work.
-    
-    // We create `MAX_THREADS` threads
-    for (int i = 0; i < MAX_THREADS; i += 1) {
+    for (int32_t i = 0; i < MAX_THREADS; i += 1) {
         pthread_join(threads[i], NULL);
     }
 
@@ -117,5 +117,6 @@ int main(void) {
     // How can we correct? :) Indice: pthread_mutex_init, pthread_cond_init
     printf("Résultat du compteur partagé: %i\n", shared_counter_value(shared_counter));
 
+    shared_counter_drop(shared_counter);
     printf("Les threads ont fini leur travail et son terminées le thread main peut maintenant se terminer\n");
 }
